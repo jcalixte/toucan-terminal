e23776df025b6b155812e5e037cc8916c168a7f2
Julien Calixte <julienc@bam.tech>
2022-12-20 16:17:30 +0100

    feat: 🎸 init

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+const SECOND = 1000
+
+const now = ref(new Date())
+
+const timeoutId = setInterval(() => {
+  now.value = new Date()
+}, SECOND)
+
+onUnmounted(() => {
+  clearInterval(timeoutId)
+})
+</script>
+
+<template>
+  <Box
+    :padding="2"
+    width="100%"
+    maxWidth="100%"
+    justifyContent="center"
+    alignItems="center"
+    borderColor="cyanBright"
+    borderStyle="round"
+  >
+    <Text>{{ now.toLocaleTimeString() }}</Text>
+  </Box>
+</template>
